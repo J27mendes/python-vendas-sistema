88fbffab6f5b4f04ddaa5c54d1b9478d0dc90a77
J27mendez <mendes_j27@hotmail.com>
2025-11-02 14:48:40 -0300

    feat: add class Produto

--- src/models/Produto.py
+++ src/models/Produto.py
@@ -1,2 +1,6 @@
 class Produto:
-    print("produto")
+    def __init__(self, id: int, nome: str, categoria: str, preco: float):
+        self.id = id
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
