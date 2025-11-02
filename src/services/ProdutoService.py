8e132141c2c8512e892931435ac44b2164b963e0
J27mendez <mendes_j27@hotmail.com>
2025-11-02 14:55:33 -0300

    feat: add class ProdutoService e metodos

--- src/services/ProdutoService.py
+++ src/services/ProdutoService.py
@@ -1,2 +1,14 @@
+from src.repositories.ProdutoRepository import ProdutoRepository
+from src.models.Produto import Produto
+
 class ProdutoService:
-   print("produto service")
+   def cadastrar_produto(self, nome: str, categoria: str, preco: float):
+      produto = Produto(None, nome, categoria, preco)  # ID será autoincrementado
+      ProdutoRepository.criar_produto(produto)
+    
+   @staticmethod
+   def carregar_dados_simulados():
+      """Carregar dados simulados"""
+      ProdutoRepository.carregar_dados_simulados()
+
+ 
