ab7bf22d7f7c0d734255955153f8578b1b1b115a
J27mendez <mendes_j27@hotmail.com>
2025-11-02 15:24:56 -0300

    feat: chamando o metodo para registrar as vendas

--- src/main.py
+++ src/main.py
@@ -5,13 +5,29 @@ import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from src.services.ProdutoService import ProdutoService
+from src.services.VendaService import VendaService
 from src.utils.Database import criar_banco_de_dados
 
 
 def main():
    criar_banco_de_dados()
+
+   ProdutoService.listar_produtos_e_ids()
     
    ProdutoService.carregar_dados_simulados()
+
+   vendas = VendaService.registrar_venda()
+
+   if vendas:
+      # Registra as vendas no banco de dados (chamada estática)
+      VendaService.registrar_vendas(vendas)
+      print("\nVendas registradas. Gerando relatórios...")
+   else:
+      print("Nenhuma venda foi registrada.")
+
+   print("\n" + "="*50)
+   print("🛒 REGISTRO DE VENDAS")
+   print("="*50)
 
 if __name__ == "__main__":
     main()
