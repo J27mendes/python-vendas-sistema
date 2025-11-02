a4e3f3b749c729ace72a34c1e4d2fbda58fe1f4b
J27mendez <mendes_j27@hotmail.com>
2025-11-02 15:28:04 -0300

    feat: add função salvar_relatorio_csv

--- src/utils/RelatorioUtils.py
+++ src/utils/RelatorioUtils.py
@@ -1,2 +1,13 @@
-def salvar_relatorio_csv(): 
-    print('salvar relatorio csv')
+import csv
+from typing import List, Tuple
+
+def salvar_relatorio_csv(dados: List[Tuple], nome_arquivo: str, cabecalho: List[str]):
+    """Função para salvar os relatórios em arquivos CSV (atualizando, não sobrescrevendo)"""
+    try:
+        with open(nome_arquivo, mode='a', newline='', encoding='utf-8') as file:
+            writer = csv.writer(file)
+            if file.tell() == 0:  # Se o arquivo estiver vazio, escreve o cabeçalho
+                writer.writerow(cabecalho)
+            writer.writerows(dados)
+    except Exception as e:
+        print(f"Erro ao salvar o CSV: {e}")
