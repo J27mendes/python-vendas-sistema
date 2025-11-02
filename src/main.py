1eab2d4b6646ebcc45769f0bb8bf4ebe98d3ae84
J27mendez <mendes_j27@hotmail.com>
2025-11-02 15:02:18 -0300

    feat: primeito commit para o main

--- src/main.py
+++ src/main.py
@@ -1,5 +1,17 @@
+import sys
+import os
+
+# Adiciona o diretório 'src' ao sys.path para garantir que o Python consiga localizar os módulos
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from src.services.ProdutoService import ProdutoService
+from src.utils.Database import criar_banco_de_dados
+
+
 def main():
-   print("main")
+   criar_banco_de_dados()
+    
+   ProdutoService.carregar_dados_simulados()
 
 if __name__ == "__main__":
     main()
