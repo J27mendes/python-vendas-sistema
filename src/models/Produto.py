2f7e044ae645dfb655b9a3ef118f6cc98c602425
J27mendez <mendes_j27@hotmail.com>
2025-11-11 14:23:16 -0300

    refactor: add Herança da Classe BaseModel

--- src/models/Produto.py
+++ src/models/Produto.py
@@ -1,9 +1,14 @@
-class Produto:
-    def __init__(self, id: int, nome: str, categoria: str, preco: float):
-        self.id = id
+from typing import Optional
+from .BaseModel import BaseModel 
+class Produto(BaseModel):
+    def __init__(self, id: Optional[int], nome: str, categoria: str, preco: float):
+        
+        super().__init__(id) 
+        
         self.nome = nome
         self.categoria = categoria
         self.preco = preco
     
     def __repr__(self):
+        
         return f"Produto(id={self.id}, nome={self.nome}, categoria={self.categoria}, preco={self.preco})"
